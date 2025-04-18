3ab3aeb8fedcc522133ce730fb772a281ccd3ab8
Filip <filip_yousif@hotmail.com>
2025-04-18 19:01:26 +0200

    Bug_Fixed
    Fixed the bug where objects which are above the character, when collision is made, the velocity increased over time.
    
    Now when the character hits an object above it the velocity in y -axis i dropped to 0 in an instance and the the gravity pulls him down

--- Assets/Scripts/PlayerMovments.cs
+++ Assets/Scripts/PlayerMovments.cs
@@ -79,4 +79,15 @@ public class PlayerMovments : MonoBehaviour
 
         rigidbody.MovePosition(position);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
+        {
+            if (transform.DotTest(collision.transform, Vector2.up))
+            {
+                velocity.y = 0f;
+            }
+        }
+    }
 }
