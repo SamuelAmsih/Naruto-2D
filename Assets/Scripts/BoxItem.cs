6a455fb8bf5fedeeca2c5488a8ffce8f90aa4478
John <john-molin@hotmail.com>
2025-05-05 15:09:41 +0200

    bb

--- Assets/Scripts/BoxItem.cs
+++ Assets/Scripts/BoxItem.cs
@@ -15,7 +15,7 @@ public class BoxItem : MonoBehaviour
         BoxCollider2D triggerCollider = GetComponent<BoxCollider2D>();
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
 
-        rigidbody.isKinematic = true;
+        rigidbody.bodyType = RigidbodyType2D.Kinematic;
         physicsCollider.enabled = false;
         triggerCollider.enabled = false;
         spriteRenderer.enabled = false;
@@ -38,7 +38,7 @@ public class BoxItem : MonoBehaviour
         }
         transform.localPosition = endLocal;
 
-        rigidbody.isKinematic = false;
+        rigidbody.bodyType = RigidbodyType2D.Dynamic;
         physicsCollider.enabled = true;
         triggerCollider.enabled = true;
     }
